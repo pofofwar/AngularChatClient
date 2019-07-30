00166e21cb4c12eae5693d60961b735835ae6bf3
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-30 18:43:27 +0300

    remove redundant imports

--- src/app/components/streams/streams.component.ts
+++ src/app/components/streams/streams.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/service/token.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-streams',
