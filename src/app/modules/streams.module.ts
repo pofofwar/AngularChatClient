2e903bf5aac28428a74eb16e270cdf9b5615e7ea
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-31 11:19:12 +0300

    add side components

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
-import { TokenService } from '../service/token.service';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { SideComponent } from '../components/side/side.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent],
   imports: [CommonModule],
   exports: [StreamsComponent]
 })
