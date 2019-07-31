1743f8c5352e2b0a6a4c43cc61d742293a9e24d6
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-31 11:35:35 +0300

    add posts form and post components

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { SideComponent } from '../components/side/side.component';
+import { PostFormComponent } from '../components/post-form/post-form.component';
+import { PostsComponent } from '../components/posts/posts.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent, SideComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent],
   imports: [CommonModule],
   exports: [StreamsComponent]
 })
